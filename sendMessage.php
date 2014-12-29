fcb18becedb532e3055d65bd7b9421b0a33ea5ca
Hardline98 <d.60wey@gmail.com>
2014-12-29 23:48:39 +0000

    Added alias for /admin
    \o/

--- sendMessage.php
+++ sendMessage.php
@@ -82,6 +82,24 @@ if($exMessage[0] == "/"){
 					file_put_contents($adminFile,$update);
 				}
 			}
+		}else if($command == "rank"){
+			$adminFile = "admin.txt";
+			$adminContents = file_get_contents($adminFile);
+			$adminSplit = explode(",",$adminContents);
+			$superUserFile = "super.txt";
+			$superContents = file_get_contents($superUserFile);
+			$superSplit = explode(",",$superContents);
+			if(strtolower($parameterOne) == "give" && !in_array($parameterTwo,$adminSplit)){
+				$update = $adminContents.",".$parameterTwo;
+				file_put_contents($adminFile,$update);
+			}else if(strtolower($parameterOne == "remove")){
+				if(!in_array($parameterTwo,$superSplit)){
+					$loc = array_search($parameterTwo,$adminSplit);
+					unset($adminSplit[$loc]);
+					$update = implode(",",$adminSplit);
+					file_put_contents($adminFile,$update);
+				}
+			}
 		}else if($command == "say"){
 			//Make multi word parameter
 			unset($parameter[0]);
